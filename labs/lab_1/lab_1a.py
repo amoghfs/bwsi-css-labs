3bb91ef06c59e052b5a2124d56cd53eb2ee84918
amoghfs <agurlahosur123@gmail.com>
2026-02-15 12:35:08 -0800

    Update robot_speed variable to 3 m/s

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,13 +3,14 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+
+This is to simulate a change made on a robot: robot_speed = 3 #m/s
 """
 
 def main():
     print("Hello World!")
 
     name = "Amogh Gurlahosur" # TODO: Insert your name between the double quotes
-    robot_speed = 5 #m/s
     print(f"{name}, Welcome to the CSS course!")
     print(f"Hi everyone! My name is {name}, and I am a junior at Archbishop Mitty High School in San Jose, California. I'm interested in machine learning and its applications in aerospace systems. I also enjoy programming and solving problems. A fun fact about me is I enjoy playing chess in my free time.")
 
