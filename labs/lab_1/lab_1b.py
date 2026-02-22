d6c6d327bf64fc1634504edb71b22c0a04b89232
amoghfs <agurlahosur123@gmail.com>
2026-02-22 10:41:13 -0800

    Added user_input function

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -36,16 +36,22 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
             raise ValueError("Cannot divide by zero.")
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
-
+def user_input(prompt):
+    while True:
+        try:
+            num = float(input(prompt))
+            return num
+        except ValueError:
+            print("Please enter a valid number")
+    
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
+    num1 = user_input("Enter the first number: ")
+    num2 = user_input("Enter the second number: ")
+    operation = input("Enter the operation (add, subtract, divide, multiply): ")  
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
     print(f"The result of {operation}ing {num1} and {num2} is: {result}")
